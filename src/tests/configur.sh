25df48e4cc1b83a2dd51640471102ce227760f23
Jiri Malak <malak.jiri@gmail.com>
2020-04-08 18:51:32 +0200

    cleanup configur.sh remove cruft from this file created as copy of src/configur.sh

--- src/tests/configur.sh
+++ src/tests/configur.sh
@@ -5,46 +5,10 @@
 # for cross-compiling Watt-32 on Linux. Targets are:
 #   djgpp, mingw32, mingw64, cygwin, clang or watcom
 #
-# What works:
-# - generates suitable target makefile from makefile.all
-# - generates the src/build/<target> objects directory
-#
-# What does not work:
-# - generation of inc/sys/<target>.err, e.g. djgpp.err, and
-#   src/build/<target>/syserr.c won't happen:
-#   util/errnos.c relies on being compiled as a target-exe:
-#   it relies on the sys_nerr value from the target libc and
-#   the strerror() string returned from target's libc.  none
-#   of these can be accomplished by a simple cross-build.
+# Generates suitable target makefile from makefile.all
 #
 
-#
-# target triplet for cross-djgpp toolchain:
-#
-DJGPP_PREFIX="i586-pc-msdosdjgpp"
-
-#
-# target triplet for cross-win64 toolchain, MinGW-w64:
-#
-MINGW64_PREFIX="x86_64-w64-mingw32"
-
-#
-# target triplet for cross-win32 toolchain, MinGW-xxx
-# for MinGW x86 targets, one can use either MinGW-w64:
-#MINGW_PREFIX="i686-w64-mingw32"
-# ... or the plain old MinGW.org:
-#
-MINGW_PREFIX="i686-pc-mingw32"
-
-#
-# target triplet for cross-cygwin toolchain:
-#
-CYGWIN_PREFIX="i686-pc-cygwin"
-
-#
-# OpenWatcom linux distributions have no prefixing.
-#
-WATCOM_PREFIX=
+MKMAKE=../../util/linux/mkmake
 
 #
 # error-out functions:
@@ -76,7 +40,7 @@ usage ()
 gen_djgpp ()
 {
   echo "Generating DJGPP makefile"
-  ../../util/linux/mkmake -o djgpp.mak makefile.all DJGPP FLAT IS_GCC
+  $MKMAKE -o djgpp.mak makefile.all DJGPP FLAT IS_GCC
 
   echo "Run GNU make to make target:"
   echo "  'make -f djgpp.mak'"
@@ -85,7 +49,7 @@ gen_djgpp ()
 gen_mingw32 ()
 {
   echo "Generating MinGW32 makefile"
-  ../../util/linux/mkmake -o MinGW32.mak makefile.all MINGW32 WIN32 IS_GCC
+  $MKMAKE -o MinGW32.mak makefile.all MINGW32 WIN32 IS_GCC
 
   echo "Run GNU make to make target:"
   echo "  'make -f MinGW32.mak'"
@@ -94,8 +58,8 @@ gen_mingw32 ()
 gen_mingw64 ()
 {
   echo "Generating MinGW64-w64 makefiles"
-  ../../util/linux/mkmake -o MinGW64_32.mak makefile.all MINGW64 WIN32 IS_GCC
-  ../../util/linux/mkmake -o MinGW64_64.mak makefile.all MINGW64 WIN64 IS_GCC
+  $MKMAKE -o MinGW64_32.mak makefile.all MINGW64 WIN32 IS_GCC
+  $MKMAKE -o MinGW64_64.mak makefile.all MINGW64 WIN64 IS_GCC
 
   echo "Run GNU make to make target(s):"
   echo "  'make -f MinGW64_32.mak'"
@@ -105,7 +69,7 @@ gen_mingw64 ()
 gen_cygwin32 ()
 {
   echo "Generating CygWin32 makefile"
-  ../../util/linux/mkmake -o CygWin.mak makefile.all CYGWIN WIN32 IS_GCC
+  $MKMAKE -o CygWin.mak makefile.all CYGWIN WIN32 IS_GCC
 
   echo "Run GNU make to make target:"
   echo "  'make -f CygWin.mak'"
@@ -114,7 +78,7 @@ gen_cygwin32 ()
 gen_cygwin64 ()
 {
   echo "Generating CygWin64 makefile"
-  ../../util/linux/mkmake -o CygWin_64.mak  makefile.all CYGWIN64 WIN64 IS_GCC
+  $MKMAKE -o CygWin_64.mak  makefile.all CYGWIN64 WIN64 IS_GCC
 
   echo "Run GNU make to make target:"
   echo "  'make -f CygWin_64.mak'"
@@ -127,8 +91,8 @@ gen_cygwin64 ()
 gen_clang ()
 {
   echo "Generating clang-cl (Win32/Win64, release/debug) makefiles, directories, errnos and dependencies"
-  ../../util/linux/mkmake -o clang_32.mak makefile.all CLANG WIN32
-  ../../util/linux/mkmake -o clang_64.mak makefile.all CLANG WIN64
+  $MKMAKE -o clang_32.mak makefile.all CLANG WIN32
+  $MKMAKE -o clang_64.mak makefile.all CLANG WIN64
 
   echo "Run GNU make to make target(s):"
   echo "  'make -f clang_32.mak'"
@@ -140,12 +104,12 @@ gen_clang ()
 gen_watcom ()
 {
   echo "Generating Watcom makefiles"
-  ../../util/linux/mkmake -o watcom_s.mak makefile.all WATCOM SMALL
-  ../../util/linux/mkmake -o watcom_l.mak makefile.all WATCOM LARGE
-  ../../util/linux/mkmake -o watcom_3.mak makefile.all WATCOM SMALL32
-  ../../util/linux/mkmake -o watcom_f.mak makefile.all WATCOM FLAT
-#  ../../util/linux/mkmake -o watcom_x.mak makefile.all WATCOM FLAT X32VM
-  ../../util/linux/mkmake -o watcom_w.mak makefile.all WATCOM WIN32
+  $MKMAKE -o watcom_s.mak makefile.all WATCOM SMALL
+  $MKMAKE -o watcom_l.mak makefile.all WATCOM LARGE
+  $MKMAKE -o watcom_3.mak makefile.all WATCOM SMALL32
+  $MKMAKE -o watcom_f.mak makefile.all WATCOM FLAT
+#  $MKMAKE -o watcom_x.mak makefile.all WATCOM FLAT X32VM
+  $MKMAKE -o watcom_w.mak makefile.all WATCOM WIN32
 
   echo "Run wmake to make target(s):"
   echo "  'make -f watcom_s.mak' for small model (16-bit)"
@@ -154,6 +118,7 @@ gen_watcom ()
   echo "  'make -f watcom_f.mak' for flat model"
 #  echo "  'make -f watcom_x.mak' for flat/X32VM model"
   echo "  'make -f watcom_w.mak' for Win32"
+}
 
 gen_all ()
 {
@@ -168,10 +133,11 @@ gen_all ()
 do_clean ()
 {
   rm -f djgpp.mak
-  rm -f watcom_{f,l,s,w,x,3}.mak
-  rm -f MinGW32.mak MinGW64.mak
+  rm -f watcom_f.mak watcom_l.mak watcom_s.mak
+  rm -f watcom_w.mak watcom_x.mak watcom_3.mak
+  rm -f MinGW32.mak MinGW64_32.mak MinGW64_64.mak
   rm -f CygWin.mak CygWin_64.mak
-  rm -f clang_{32,64}.mak
+  rm -f clang_32.mak clang_64.mak
 }
 
 #
