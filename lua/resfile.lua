0596b279634c044ec136bb7c7fc08987eaff07b1
Jason Lethbridge <lethbridgejason@gmail.com>
2024-08-04 12:45:06 +1000

    Added string pattern argument to resfile.lua

--- lua/resfile.lua
+++ lua/resfile.lua
@@ -7,11 +7,14 @@
 	The script can concatinate += operators of the value together but
 	can not resolve symbols on shell executions of variables
 
-	Usage: lua resfile.lua [makefile] [variable] (> [output file])
+	The optional argument pattern allows the return variable to be
+	wrapped in a string. Any instance of ~ will be replaced.
+
+	Usage: lua resfile.lua [makefile] [variable] ([pattern]) (> [output file])
 ]]
 
 -- Need two arguments to run
-if #arg ~= 2 then os.exit(1) end
+if #arg < 2 then os.exit(1) end
 
 -- Check file can be open and read
 local file = io.open(arg[1], "r")
@@ -20,14 +23,16 @@ if not file then os.exit(1) end
 -- Read the file line by line to fully construct the value
 local var = ""
 for line in file:lines() do
-    local val = line:match("^" .. arg[2] .. "%s*=%s*(.*)")
-    if val then var = val else
-        val = line:match("^" .. arg[2] .. "%s*%+=%s*(.*)")
-        if val then var = var .. ' ' .. val end
-    end
+	local val = line:match("^" .. arg[2] .. "%s*=%s*(.*)")
+	if val then var = val else
+		val = line:match("^" .. arg[2] .. "%s*%+=%s*(.*)")
+		if val then var = var .. ' ' .. val end
+	end
 end
+file:close()
 
--- Close the file, output the result
-makefile:close()
+-- Wrap in string if applicable
+if #arg >= 3 then var = string.gsub(arg[3], "~", var) end
 print(var)
+
 os.exit(0)
